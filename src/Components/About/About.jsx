edbb867828ee0dad418660e297fe3bd1cb86a832
Arnab Ghosh <ajg616725@gmail.com>
2023-11-24 19:27:28 +0530

    SERVICES

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -15,61 +15,83 @@ import delivery from "../../asset/Laundry-purple-top.gif";
 const About = () => {
   return (
     <div className="about-container">
-        <div className="about">
-      <div className="choseUsSection">
-        <p>Why Choose Us?</p>
-      </div>
-      <div className="about-box-container">
-        <AboutBox
-          icon={<img src={about1} alt="Pick up" />}
-          text="Explore our carefully curated selection of sustainable products, each
+      <div className="about">
+        <div className="choseUsSection">
+          <p>Why Choose Us?</p>
+        </div>
+        <div className="about-box-container">
+          <AboutBox
+            icon={<img src={about1} alt="Pick up" />}
+            text="Explore our carefully curated selection of sustainable products, each
           designed to reduce your carbon footprint"
-          heading="Sustainable Products"
-        />
-        <AboutBox
-          icon={<img src={about2} alt="Pick up" />}
-          text="Make conscious choices with our 
+            heading="Sustainable Products"
+          />
+          <AboutBox
+            icon={<img src={about2} alt="Pick up" />}
+            text="Make conscious choices with our 
          eco-friendly products, knowing 
          that your purchases promote ethical 
          sourcing and responsible 
          manufacturing practices."
-          heading="Affordable pricing"
-        />
-        <AboutBox
-          icon={<img src={about3} alt="Pick up" />}
-          text="Invest in long-lasting and reliable 
+            heading="Affordable pricing"
+          />
+          <AboutBox
+            icon={<img src={about3} alt="Pick up" />}
+            text="Invest in long-lasting and reliable 
         products that meet our stringent 
         quality standards, ensuring your 
         satisfaction and the longevity of 
         your purchases."
-          heading="High-Quality Selection"
-        />
-        <AboutBox
-          icon={<img src={about4} alt="Pick up" />}
-          text="Our sustainable packaging ensures 
+            heading="High-Quality Selection"
+          />
+          <AboutBox
+            icon={<img src={about4} alt="Pick up" />}
+            text="Our sustainable packaging ensures 
         that your orders arrive safely 
         while minimizing their impact on 
         the planet."
-          heading="Sustainable Packaging"
-        />
+            heading="Sustainable Packaging"
+          />
+        </div>
       </div>
-    </div>
-    <div className="process">
-    <div className="how-we-work">
+      <div className="process">
+        <div className="how-we-work">
           <h2>How We Work</h2>
-      </div>
-      <div className="process-animation">
-      <img src={pickup}/>
-        <img src={wash}/>
-        <img src={iron}/>
-        <img src={delivery}/> 
-      </div>
-     <div>
+        </div>
+        <div className="process-animation">
+          <img src={pickup} />
+          <img src={wash} />
+          <img src={iron} />
+          <img src={delivery} />
+        </div>
+        <div className="stepper">
+          <div className="stepper-box">
+          <h3>Pickup</h3>
+          <div className="line">
 
-     </div>
-    </div>
-    </div>
+          </div>
+          </div>
+          <div className="stepper-box">
+          <h3>Wash & Dry</h3>
+          <div className="line">
 
+          </div>
+          </div>
+          <div className="stepper-box">
+          <h3>Iron & Fold</h3>
+          <div className="line">
+
+          </div>
+          </div>
+          <div className="stepper-box">
+          <h3>Deliver</h3>
+          {/* <div className="line">
+
+          </div> */}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/Components/home/Steps.jsx
+++ src/Components/home/Steps.jsx
@@ -7,34 +7,121 @@ import washdry from "../../asset/Laundry and dry cleaning.gif"
 
 const Steps = () => {
   return (
-    <section className="steps">
-      <div className="headings">
-        <h2>HOW IT WORKS?</h2>
-        <h1>Get it done in 4 steps</h1>
-      </div>
-      <div className="boxes">
-        <Box number={1} heading="Pick up" icon={<img src={lwda} alt="Pick up"/> } />
-        <Box number={2} heading="Wash & Dry" icon={<img src={washdry} alt="Pick up"/>} />
-        <Box number={3} heading="Fold & Iron" icon={<img src={iron} alt="Pick up" />} />
-        <Box number={4} heading="Delivery" icon={<img src={delivery} alt="Pick up"/>}/>
+    // <section className="steps">
+    //   <div className="headings">
+    //     <h2>HOW IT WORKS?</h2>
+    //     <h1>Get it done in 4 steps</h1>
+    //   </div>
+    //   <div className="boxes">
+    //     <Box number={1} heading="Pick up" icon={<img src={lwda} alt="Pick up"/> } />
+    //     <Box number={2} heading="Wash & Dry" icon={<img src={washdry} alt="Pick up"/>} />
+    //     <Box number={3} heading="Fold & Iron" icon={<img src={iron} alt="Pick up" />} />
+    //     <Box number={4} heading="Delivery" icon={<img src={delivery} alt="Pick up"/>}/>
         
-      </div>
-    </section>
+    //   </div>
+    // </section>
+    <section class="services">
+
+    <div class="s-box-container">
+  
+    <div class="s-box">
+ 
+    <div class="bar"></div>
+
+    <img alt="1" src="pexels-web-donut-19090.jpg" />
+  
+    <h1>Spend Money</h1>
+ 
+        <p>Track on your all daily expense and make your day to life easier.</p>
+
+    <a class="s-btn" href="#">More</a>
+    </div>
+ 
+    <div class="s-box">
+
+    <div class="bar"></div>
+   
+    <img alt="2" src="pexels-web-donut-19090.jpg" />
+  
+    <h1>Set Budget</h1>
+    
+        <p>Make a Budget That Fit Right With You And Can Do Most With It</p>
+  
+    <a class="s-btn" href="#">More</a>
+    </div>
+        
+    <div class="s-box">
+   
+    <div class="bar"></div>
+    
+    <img alt="3" src="pexels-web-donut-19090.jpg" />
+   
+    <h1>Security</h1>
+
+        <p>We Store All Card Info And Money Safe To Make You Relax And Crazy</p>
+ 
+    <a class="s-btn" href="#">More</a>
+    </div>
+    </div>
+    
+      
+        <div class="s-box-container">
+     
+        <div class="s-box">
+     
+        <div class="bar"></div>
+ 
+        <img alt="1" src="pexels-web-donut-19090.jpg" />
+     
+        <h1>Spend Money</h1>
+     
+            <p>Track on your all daily expense and make your day to life easier.</p>
+     
+        <a class="s-btn" href="#">More</a>
+        </div>
+        
+        <div class="s-box">
+      
+        <div class="bar"></div>
+      
+        <img alt="2" src="pexels-web-donut-19090.jpg" />
+  
+        <h1>Set Budget</h1>
+     
+            <p>Make a Budget That Fit Right With You And Can Do Most With It</p>
+   
+        <a class="s-btn" href="#">More</a>
+        </div>
+      
+        <div class="s-box">
+      
+        <div class="bar"></div>
+      
+        <img alt="3" src="pexels-web-donut-19090.jpg" />
+    
+        <h1>Security</h1>
+      
+            <p>We Store All Card Info And Money Safe To Make You Relax And Crazy</p>
+       
+        <a class="s-btn" href="#">More</a>
+        </div>
+        </div>
+        </section>
   );
 };
 
 export default Steps;
 
-const Box = ({ number, heading, icon }) => {
-  return (
-    <>
-      <div className="box">
-        <div className="box-icon abs">
-          <h4>{number}</h4>
-        </div>
-        <h3>{heading}</h3>
-        <div className="box-icon">{icon}</div>
-      </div>
-    </>
-  );
-};
+// const Box = ({ number, heading, icon }) => {
+//   return (
+//     <>
+//       <div className="box">
+//         <div className="box-icon abs">
+//           <h4>{number}</h4>
+//         </div>
+//         <h3>{heading}</h3>
+//         <div className="box-icon">{icon}</div>
+//       </div>
+//     </>
+//   );
+// };
